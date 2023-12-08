2997a3681c6f527fad8908cc87b284126c039ab0
xelA-S <asmith6398@gmail.com>
2023-12-08 00:38:57 +0000

    Add project files.

---
+++ NetworkPinger/PingService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkPinger
+// when seeing the word "service" consider this as an abstraction, an attempt to clean up the code being used
+// a class is the creation of an object, this is a reference type
+// used to collect variables
+{
+    // classes have properties and fields
+    public class PingService
+    {
+        // these are properties of the class
+        // this design pattern is simple but not optimal
+        // the properties below are in state, methods are used to manipulate these properties
+        public string Data { get; set; }
+        public byte[] Buffer { get; set; }
+
+        public int Timeout { get; set; }
+
+        public string Address { get; set; }
+
+        public Ping pingSender { get; set; }
+
+        public PingOptions pingOptions { get; set; }
+
+        // the properties above are in state, the method below is used to manipulate these properties
+        // this is the constructor
+        // these values are assigned when the object is instansiated
+        public PingService()
+        {
+            Timeout = 120;
+            Address = "4.2.2.2";
+            Buffer = Encoding.ASCII.GetBytes("Learn to code");
+            pingSender = new Ping();
+            pingOptions = new PingOptions();
+            pingOptions.DontFragment = true;
+        }
+
+        public bool SendPing()
+        {
+            PingReply reply = pingSender.Send(Address, Timeout, Buffer, pingOptions);
+            if (reply.Status == IPStatus.Success)
+            {
+                Console.WriteLine($"Response: {reply.Status.ToString()}");
+                Console.WriteLine($"RoundTrip: {reply.RoundtripTime}");
+                Console.WriteLine($"Time to live: {reply.Options.Ttl}");
+                Console.WriteLine($"Buffer size: {reply.Buffer.Length}");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ NetworkPinger/Program.cs
@@ -0,0 +1,44 @@
+﻿using NetworkPinger;
+using System.Net.NetworkInformation;
+using System.Text;
+
+// Pinging Google DNS Server 4.2.2.2
+
+// this is used to create an instance of the ping function and configure the ping options
+Ping pingSender = new Ping();
+PingOptions options = new PingOptions();
+
+// sometimes when a certain amount is sent, it may cause an error. this is to prevent that
+options.DontFragment = true;
+
+// this is the data what will be sent
+string data = "Learn to code";
+
+// this is so the data is actually recieved, must be a byte array, consists of any number below 255
+byte[] buffer = Encoding.ASCII.GetBytes(data);
+
+int timeout = 120;
+string address = "4.2.2.2";
+// this performs the ping then returns the data into an object
+PingReply reply = pingSender.Send(address, timeout, buffer, options);
+
+// this checks whether the ping was successful by comparing it the status response used for success
+if (reply.Status == IPStatus.Success)
+{
+    Console.WriteLine($"Response: {reply.Status.ToString()}");
+    Console.WriteLine($"RoundTrip: {reply.RoundtripTime}");
+    Console.WriteLine($"Time to live: {reply.Options.Ttl}");
+    Console.WriteLine($"Buffer size: {reply.Buffer.Length}");
+
+
+
+
+}
+
+// using the PingService Object
+
+PingService pingService = new PingService();
+
+var response = pingService.SendPing();
+
+Console.WriteLine(response);
